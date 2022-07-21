14b6138a13e1a97fec493c7b31c143032fb475c0
Anthony Pena <apena5@alumni.jh.edu>
2022-07-21 16:29:54 -0400

    feat: changed port number

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-const PORT = 3000;
+const PORT = 4000;
  
 app.listen(PORT, ()=>{
   console.log(`Listening for requests on port ${PORT}`);
