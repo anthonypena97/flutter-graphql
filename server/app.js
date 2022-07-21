af290ed5f2836bfa7112154ba9aa1d87cc398a53
Anthony Pena <apena5@alumni.jh.edu>
2022-07-21 16:28:08 -0400

    chore: testing git

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-const PORT = 4000;
+const PORT = 3000;
  
 app.listen(PORT, ()=>{
   console.log(`Listening for requests on port ${PORT}`);
