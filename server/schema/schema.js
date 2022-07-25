22c882398206a94be94a14ceadea9d6518e467e9
Anthony Pena <apena5@alumni.jh.edu>
2022-07-25 13:25:39 -0400

    doc: added dummy data to schema.js

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,5 +1,14 @@
 const {GraphQLObjectType, GraphQLID, GraphQLString, GraphQLInt, GraphQLSchema, buildSchema} = require('graphql');
 
+// dummy data
+var userData = [
+  {id: '1', name: 'Bond', age: 36},
+  {id: '13', name: 'Anna', age: 26},
+  {id: '211', name: 'Bella', age: 16},
+  {id: '19', name: 'Gina', age: 26},
+  {id: '150', name: 'Georgina', age: 36},
+];
+
 // create types
 const UserType = new GraphQLObjectType({
   name: 'User',
