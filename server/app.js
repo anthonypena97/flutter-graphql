e64c03eac748d5f4f0be8388ef6e160e60bd3309
Anthony Pena <apena5@alumni.jh.edu>
2022-07-26 16:22:56 -0400

    chore: installed dotenv in the server directory

--- server/app.js
+++ server/app.js
@@ -1,9 +1,12 @@
-require('dotenv').config()
+const dotenv = require('dotenv')
+console.log(process.env) 
 
 const express = require('express');
 const {graphqlHTTP} = require('express-graphql');
 
 const schema = require('./schema/schema');
+
+const mongoose = require('mongoose');
 
 const app = express();
 
@@ -15,6 +18,10 @@ app.use('/graphql', graphqlHTTP({
 }));
 
 const PORT = 4000;
+
+// console.log(process.env.MONGO_USERNAME)
+
+mongoose.connect(`mongodb+srv://anthonypena97:<password>@graphqlcluster.6k90h.mongodb.net/?retryWrites=true&w=majority`)
  
 app.listen(PORT, ()=>{
   console.log(`Listening for requests on port ${PORT}`);
