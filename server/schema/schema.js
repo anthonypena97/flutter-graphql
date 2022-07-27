08ffd96a88217ca9cc35ac1fcbb3bc1a12dc9439
Anthony Pena <apena5@alumni.jh.edu>
2022-07-27 12:16:26 -0400

    feat: RemovePost and RemoveHobby mutations

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -236,6 +236,20 @@ const Mutation = new GraphQLObjectType({
       }
     },
     
+    RemovePost:{
+      type: PostType,
+      args: {
+        id: {type: new GraphQLNonNull(GraphQLString)},
+      },
+      resolve(parent,args){
+        let removedPost = Post.findByIdAndRemove(args.id).exec();
+        if(!removedPost){
+          throw new "Error" ()
+        }
+        return removedPost;
+      }
+    },
+    
     createHobby: {
       type: HobbyType,
       args: {
@@ -272,6 +286,20 @@ const Mutation = new GraphQLObjectType({
           },
           {new: true}
         ))
+      }
+    },
+    
+    RemoveHobby:{
+      type: HobbyType,
+      args: {
+        id: {type: new GraphQLNonNull(GraphQLString)},
+      },
+      resolve(parent,args){
+        let removedHobby = Hobby.findByIdAndRemove(args.id).exec();
+        if(!removedHobby){
+          throw new "Error" ()
+        }
+        return removedHobby;
       }
     },
     
