2afcfca642944b089e87166be4b3e1adaad20c1a
Anthony Pena <apena5@alumni.jh.edu>
2022-07-29 16:25:39 -0400

    chore: updated client folder to clear errors

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,20 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 
-void main() {
-  runApp(const MyApp());
+void  main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initHiveForFlutter();
+  
+   final HttpLink link = HttpLink(
+    'https://graphql-flutter-hobbies.herokuapp.com/graphql',
+  );
+  
+  ValueNotifier<GraphQLClient> client = ValueNotifier(
+    GraphQLClient(
+      link: link,
+      // The default store is the InMemoryStore, which does NOT persist to disk
+      cache: GraphQLCache(store: HiveStore()),
+    ),
+  );
+  
+  runApp(MyApp(client: client));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  final ValueNotifier<GraphQLClient> client;
+
+  const MyApp({super.key, required this.client});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return GraphQLProvider(
+      client: client,
+      child: CacheProvider(
+        child: MaterialApp(
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home: const HomeScreen()
+        ),
       ),
-      home: const HomeScreen()
     );
   }
 }
