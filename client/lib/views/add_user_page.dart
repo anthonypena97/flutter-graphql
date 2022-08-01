187349bf02d42140341bf1182c57cddd2cd999c2
Anthony Pena <apena5@alumni.jh.edu>
2022-08-01 17:22:22 -0400

    feat: started add a user page with scaffold and mutation wrapper

--- client/lib/views/add_user_page.dart
+++ client/lib/views/add_user_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 
 class AddUserPage extends StatefulWidget {
   const AddUserPage({Key? key}) : super(key: key);
@@ -8,9 +9,59 @@ class AddUserPage extends StatefulWidget {
 }
 
 class _AddUserPageState extends State<AddUserPage> {
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
-    return Container();
-    
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Add a User',
+          style: TextStyle(
+            color: Colors.grey,
+            fontSize: 19,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        backgroundColor: Colors.lightGreen,
+        elevation: 0,
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.all(24),
+          margin: const EdgeInsets.symmetric(horizontal: 24, vertical: 6),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(16),
+            boxShadow: [
+              BoxShadow(
+                  offset: Offset(0, 10),
+                  color: Colors.grey.shade300,
+                  blurRadius: 30),
+            ],
+          ),
+          child: Column(
+            children: [
+              Mutation(
+                options: MutationOptions(
+                  document: gql(insertUser()),
+                  fetchPolicy: FetchPolicy.noCache,
+                  onCompleted: (data) {},
+                ),
+                builder: (runMutation, result) {
+                  return Form(
+                    key: _formKey,
+                    child: Column(),
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
+}
+
+String insertUser() {
+  return "";
 }
