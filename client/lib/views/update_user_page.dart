387af3a24ab72a6f09ed398ba35709a1abfc1b02
Anthony Pena <apena5@alumni.jh.edu>
2022-08-12 23:50:13 -0600

    feat: passing query into update_user_page

--- client/lib/views/update_user_page.dart
+++ client/lib/views/update_user_page.dart
@@ -1,9 +1,11 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 class UpdateUser extends StatefulWidget {
   final String id;
   final String name;
-  final String age;
+  final int age;
   final String profession;
   
   const UpdateUser(
@@ -22,6 +24,38 @@ class UpdateUser extends StatefulWidget {
 class _UpdateUserState extends State<UpdateUser> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: Text(
+          "Update ${widget.name}",
+          style: const TextStyle(
+            color: Colors.grey, fontSize: 19, fontWeight: FontWeight.bold
+          ),
+        )
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.all(24.0),
+          margin: const EdgeInsets.symmetric(
+            horizontal: 24.0,
+            vertical: 6.0,
+          ),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(16.0),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.shade300,
+                offset: const Offset(0,10.0),
+                blurRadius: 30.0,
+              ),
+            ],
+          ),
+          child: Center(child: Text(widget.id))
+        )
+      ),
+    );
   }
 }
--- client/lib/views/users_page.dart
+++ client/lib/views/users_page.dart
@@ -14,6 +14,7 @@ class _UsersPageState extends State<UsersPage> {
   final String _query = """
   query{
     users{
+      id
       name
       profession
       age
